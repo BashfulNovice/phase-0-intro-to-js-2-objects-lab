3a9d7ae96a27d32db5443f53a71fd7e2314a0f6d
Todd Krieger <twkriege@mtu.edu>
2021-09-09 16:09:17 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+
+var employee = {
+    name: "",
+    address: ""
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+   var newEmp = {...employee};
+   newEmp[key] = value;
+   return newEmp;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    var newEmp = {...employee};
+    delete newEmp[key]; 
+    return newEmp
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+
